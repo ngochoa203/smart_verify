da959eada73577b79970f4ef9bb4303e180ea6d4
ngochoa203 <hongochoa203@gmail.com>
2025-04-23 00:14:42 +0700

    Update

--- fe/lib/main.dart
+++ fe/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'screens/HomePage.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -8,36 +10,6 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: EnglishLearningScreen());
-  }
-}
-
-class EnglishLearningScreen extends StatelessWidget {
-  const EnglishLearningScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('English Learning App')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Welcome to English Learning App!',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to lessons or other features
-              },
-              child: const Text('Start Learning'),
-            ),
-          ],
-        ),
-      ),
-    );
+    return MaterialApp(home: HomePage());
   }
 }
