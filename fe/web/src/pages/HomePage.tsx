c75dd76310ee0aed72c7518b1223ea9ebbd5e51c
ngochoa203 <hongochoa203@gmail.com>
2025-06-12 22:18:50 +0700

    Updated banner

--- fe/web/src/pages/HomePage.tsx
+++ fe/web/src/pages/HomePage.tsx
@@ -1,15 +1,34 @@
-import Banner from "../components/Banner";
-import Header from "../components/MainHeader";
+import HeroBanner from "../components/HeroBanner";
+import CategoryGrid from "../components/CategoryGrid";
+import FlashSale from "../components/FlashSale";
+import PromotionBanner from "../components/PromotionBanner";
+import ProductGrid from "../components/ProductGrid";
 
 export default function HomePage() {
     return (
-        <div>
-            <Header />
-            <div className="w-full max-h-[1600px] mx-auto px-4">
-                <Banner />
+        <div className="bg-gradient-to-br from-gray-50 via-blue-50 to-purple-50">
+            {/* Hero Banner Section */}
+            <div className="max-w-7xl mx-auto px-4 pt-8">
+                <HeroBanner />
             </div>
-            <div className="category-list">
-                <span>Danh muc</span>
+            
+            {/* Main Content */}
+            <div className="max-w-7xl mx-auto px-4 py-8 space-y-8">
+                {/* Category Grid */}
+                <CategoryGrid />
+                
+                {/* Flash Sale */}
+                <FlashSale />
+                
+                {/* Promotion Banners */}
+                <PromotionBanner />
+                
+                {/* Product Sections */}
+                <ProductGrid title="🔥 Sản phẩm bán chạy" />
+                <ProductGrid title="⭐ Được đánh giá cao" />
+                <ProductGrid title="💻 Điện tử - Công nghệ" />
+                <ProductGrid title="👕 Thời trang" />
+                <ProductGrid title="🏠 Gia dụng" />
             </div>
         </div>
     )
